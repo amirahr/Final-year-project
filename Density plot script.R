bdbac1fe423461cbd12988db1034a7fb3e44508a
Amirah Rafique <64033680+amirahr@users.noreply.github.com>
2021-04-22 22:07:42 +0100

    Update Density plot script.R

--- Density plot script.R
+++ Density plot script.R
@@ -4,6 +4,8 @@ library(readxl)
 library(scales)
 library(extrafont)
 
+df <- read_excel("Desktop/distribution plots.xlsx")
+
 p <- ggplot(df, aes(x=`Sfg-spg_residuals`)) + geom_density(alpha=.3, fill="#1B9E77") + ylab("Density") + xlab("Residuals") + theme( 
   panel.background = element_rect(fill = "white", colour = "white",
                                   size = 2, linetype = "solid"),
