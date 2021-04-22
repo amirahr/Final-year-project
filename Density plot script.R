5ee4ab87dd835fad0d0f0f3f87b32604dbd37c9d
Amirah Rafique <64033680+amirahr@users.noreply.github.com>
2021-04-22 22:06:59 +0100

    Add files via upload

---
+++ Density plot script.R
@@ -0,0 +1,17 @@
+library(ggplot2)
+library(tidyverse)
+library(readxl)
+library(scales)
+library(extrafont)
+
+p <- ggplot(df, aes(x=`Sfg-spg_residuals`)) + geom_density(alpha=.3, fill="#1B9E77") + ylab("Density") + xlab("Residuals") + theme( 
+  panel.background = element_rect(fill = "white", colour = "white",
+                                  size = 2, linetype = "solid"),
+  panel.grid.major = element_line(size = 0.25, linetype = 'solid',
+                                  colour = "grey84"), 
+  panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
+                                  colour = "grey84"),
+  plot.background = element_rect(fill = "grey96"),
+  panel.border = element_rect(colour = "black", fill=NA, size=0.5),
+  text = element_text(family="Helvetica", size=12)
+)
