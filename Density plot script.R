3ddce41e1d274f9791d9a65dce614302b8da9e9e
Amirah Rafique <64033680+amirahr@users.noreply.github.com>
2021-04-25 17:40:23 +0100

    Update Density plot script.R

--- Density plot script.R
+++ Density plot script.R
@@ -6,7 +6,9 @@ library(extrafont)
 
 df <- read_excel("Desktop/distribution plots.xlsx")
 
-p <- ggplot(df, aes(x=`Sfg-spg_residuals`)) + geom_density(alpha=.3, fill="#1B9E77") + ylab("Density") + xlab("Residuals") + theme( 
+#residuals
+p <- ggplot(df, aes(x=`Sfg-spg_residuals`)) + geom_density(alpha=.3, fill="#1B9E77") + ylab("Density") + xlab("Residuals") + 
+scale_y_continuous(limits = c(0, 0.20)) + scale_x_continuous(limits = c(-5, 7)) + theme( 
   panel.background = element_rect(fill = "white", colour = "white",
                                   size = 2, linetype = "solid"),
   panel.grid.major = element_line(size = 0.25, linetype = 'solid',
@@ -17,3 +19,21 @@ p <- ggplot(df, aes(x=`Sfg-spg_residuals`)) + geom_density(alpha=.3, fill="#1B9E
   panel.border = element_rect(colour = "black", fill=NA, size=0.5),
   text = element_text(family="Helvetica", size=12)
 )
+p
+
+#gene number
+p <- ggplot(df, aes(x=`Sfg-spg_gene`)) + geom_density(alpha=.3, fill="#1B9E77") + ylab("Density") + xlab("Gene number") + 
+scale_x_continuous(limits = c(-2000, 10000, by = 1000)) + scale_y_continuous(limits = c(0, 0.00025)) + theme( 
+     panel.background = element_rect(fill = "white", colour = "white",
+                                     size = 2, linetype = "solid"),
+     panel.grid.major = element_line(size = 0.25, linetype = 'solid',
+                                     colour = "grey84"), 
+     panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
+                                     colour = "grey84"),
+     plot.background = element_rect(fill = "grey96"),
+     panel.border = element_rect(colour = "black", fill=NA, size=0.5),
+     text = element_text(family="Helvetica", size=12)
+ )
+ p
+
+#genome size
